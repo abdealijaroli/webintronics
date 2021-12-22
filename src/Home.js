c8cdd0bc8113ab124c739fc91a82851b2ef974f0
abdealijaroli <abdealiburu03@gmail.com>
2021-12-22 11:17:48 +0530

    home page completed

--- src/Home.js
+++ src/Home.js
@@ -8,28 +8,37 @@ function Home() {
                <span className="header-nav">
                   <p>Contact</p>
                   <p>Career</p>
-                  <p>Media</p><img src="assets/down-arrow.svg" className="down-arrow"></img>
+                  <p>Media</p><img src="assets/down-arrow.svg" className="down-arrow" alt=""></img>
                </span>
             </div>
          </header>
          <div className="navbar">
             <div className="navbar-content">
-               <img src="assets/logo.svg"></img>
+               <img src="assets/logo.svg" alt=""></img>
                <div className="nav-opt">
                   <span className="hover">About</span>
                   <span className="hover">Locate Store</span>
-                  <span>Scooters</span><img style={{ width: '10px', margin: '1px 0 0 2px' }} src="assets/down-arrow.svg"></img>
-                  <span>Purchase Assistance</span><img style={{ width: '10px', margin: '1px 0 0 2px' }} src="assets/down-arrow.svg"></img>
+                  <span>Scooters</span><img style={{ width: '10px', margin: '1px 0 0 2px' }} src="assets/down-arrow.svg" alt=""></img>
+                  <span>Purchase Assistance</span><img style={{ width: '10px', margin: '1px 0 0 2px' }} src="assets/down-arrow.svg" alt=""></img>
                   <span className="btn">Dealership</span>
                </div>
             </div>
          </div>
          <div className="row">
             <div className="col-1">
-
+               <img src="assets/circle.gif" className="circle" alt=""></img>
+               <img src="assets/scooter.svg" className="scooter" alt=""></img>
             </div>
             <div className="col-2">
-
+               <h1>
+                  A neat<br/>
+                  headline<br/>
+                  comes here !
+               </h1>
+               <p>
+               A neat sample text to come here super soon. Till then admire the authentic and delightful User Interface Design of the Website. We are glad you came here , thanks for visiting . 
+               </p>
+               <span className="show-btn">Show Details</span>
             </div>
          </div>
       </div>
